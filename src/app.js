08f228d4d5e4d3809dec9a13ea0fc4594957802f
Bogdan Kochurov <kochurov@kochurov.local>
2020-11-15 11:35:58 +0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import { config } from './config/config';
+
+const app = express();
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
