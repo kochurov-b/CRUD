426625964040823f75739235c69d57f05dc2df29
Bogdan Kochurov <kochurov@kochurov.local>
2020-11-15 19:57:14 +0400

    create mutation updateUser

--- api/src/graphql/Users/Users.mutation.js
+++ api/src/graphql/Users/Users.mutation.js
@@ -12,6 +12,25 @@ const createUser = async (_, { input: userData }) => {
   }
 };
 
+const updateUser = async (_, { id, input: updated }) => {
+  try {
+    const conditions = { _id: id };
+
+    return await User.findByIdAndUpdate(
+      conditions,
+      updated,
+      (error, result) => {
+        if (error) throw Error(error);
+
+        return result;
+      },
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const mutation = {
   createUser,
+  updateUser,
 };
